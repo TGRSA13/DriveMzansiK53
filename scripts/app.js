825dc35959e4da18f703c72345de1f7586b1929a
Thobeka Mchunu <tgmchunu@gmail.com>
2024-10-22 18:48:46 +0200

    new files

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
-
-// Define the port
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+    res.send('Welcome to Drive Mzansi!');
 });
 
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
