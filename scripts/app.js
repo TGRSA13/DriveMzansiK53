9ae3991d5cae6c0cc86848f5e778712a0c07f2e3
Thobeka Mchunu <tgmchunu@gmail.com>
2024-10-22 18:50:42 +0200

    new files on public folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the home page
 app.get('/', (req, res) => {
-    res.send('Welcome to Drive Mzansi!');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// Route for vehicle controls page
+app.get('/controls', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/controls.html'));
+});
+
+// Route for rules of the road page
+app.get('/rules', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/rules.html'));
+});
+
+// Route for road signs page
+app.get('/signs', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/signs.html'));
+});
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
