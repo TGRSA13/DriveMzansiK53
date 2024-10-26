6f8cc241233c92cef8dd1edd30ae563737ec575a
Thobeka Mchunu <tgmchunu@gmail.com>
2024-10-26 12:32:46 +0200

    Set up Firebase Hosting with GitHub Actions

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'DriveMzansiK53',
+  location: 'us-central1'
+};
+
