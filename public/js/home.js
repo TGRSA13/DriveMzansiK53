04bd085d112b2f7e8816db3409ef147325a7dbfd
Thobeka Mchunu <tgmchunu@gmail.com>
2024-10-30 19:51:20 +0200

    new file

---
+++ public/js/home.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js';
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AiZaSyCglZk-B-Phojvp31xGxxxACfyO4w6lges",
+    authDomain: "drive-mzansi-app.firebaseapp.com",
+    projectId: "drive-mzansi-app",
+    storageBucket: "drive-mzansi-app.appspot.com",
+    messagingSenderId: "1007195133421",
+    appId: "1:1007195133421:web:1b7ec3cd063a31c05543e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
